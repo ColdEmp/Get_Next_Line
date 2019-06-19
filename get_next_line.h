d5207172a73551c31b5e69dd14e406fda872511a
Cameron Glanville <cglanvil@c3r6s8.wethinkcode.co.za>
2019-06-19 12:05:46 +0200

    updated

--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,14 @@
 /*   By: cglanvil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 12:53:35 by cglanvil          #+#    #+#             */
-/*   Updated: 2019/06/18 12:53:46 by cglanvil         ###   ########.fr       */
+/*   Updated: 2019/06/18 15:17:55 by cglanvil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 5;
+# include "includes/libft.h"
 
+int	ft_get_next_line
+#endif
