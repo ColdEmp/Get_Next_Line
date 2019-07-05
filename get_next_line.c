a016ea3d787592f98a689b2b760403e5adaa16ea
Cameron Glanville <cglanvil@c3r6s8.wethinkcode.co.za>
2019-07-05 15:33:13 +0200

    gets one line

--- get_next_line.c
+++ get_next_line.c
@@ -6,40 +6,70 @@
 /*   By: cglanvil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 12:18:08 by cglanvil          #+#    #+#             */
-/*   Updated: 2019/07/04 16:22:53 by cglanvil         ###   ########.fr       */
+/*   Updated: 2019/07/05 15:32:14 by cglanvil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char*	next_line(char *buff, char **line)
+{
+	char	*shard;
+	int		i;
+	int		nl;
+
+	i = 0;
+	nl = 0;
+	shard = strdup((char const*)buff);
+	while (i < (BUFF_SIZE + 1))
+	{
+		if (shard[i] == '\n' || nl)
+		{
+			shard[i] = '\0';
+			nl = 1;
+		}
+		i++;
+	}
+	*line = ft_strjoin(*line, (char const*)shard);
+	buff = (ft_strchr((char const*)buff, '\n') + 1);
+	return (buff);
+}
+
 int	get_next_line(const int fd, char **line)
 {
-	static char		*buff[BUFF_SIZE + 1];
+	static char		buff[BUFF_SIZE + 1];
 	char			*temp;
 	int				ret;
-	int				i;
-	int				j;
 
 	if (!line || fd < 0 || read(fd, NULL, 0) < 0 || BUFF_SIZE < 1)
 		return (-1);
-	i = 0;
-	j = 0;
 	*line = strdup("");
+	if (buff[0] != '\0')
+	{
+		if (ft_strchr((char
+		buff = next_line(buff,
+	}
 	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
 	{
-		buff[ret] = "\0";
 		temp = *line;
-		ft_strjoin(*line, (char const*)buff);
-		free(temp);
 		if (ft_strchr((char const*)buff, '\n'))
+		{
+			buff = next_line(buff, line);
+			free(temp);
 			break ;
+		}
+		*line = ft_strjoin(*line, (char const*)buff);
+		free(temp);
 	}
 	if (ret < 0)
 		return (-1);
 	else if (ret == 0)
-	{
-		free(buff);
 		return (0);
-	}
 	return (1);
 }
+
+
+//str = "abduraghmaan";
+
+//str[3];
+//str = (*str+3);
--- main.c
+++ main.c
@@ -6,17 +6,19 @@
 /*   By: cglanvil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 11:21:36 by cglanvil          #+#    #+#             */
-/*   Updated: 2019/07/04 14:48:49 by cglanvil         ###   ########.fr       */
+/*   Updated: 2019/07/05 10:48:28 by cglanvil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <fcntl.h>
 
 int	main(int argc, char *argv[])
 {
 	int		fd;
 	char	*line;
-
+	
+	//line = (char *)malloc(1);
 	if (argc == 2)
 	{
 		fd = open(argv[1], 'r');
